7f33712e317f0d5a5543780625a240a6fa7c6a63
saidousouare <souare_s@hotmail.com>
2016-05-24 22:00:11 +0200

    Creating model to work with

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Book(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Sheet(models.Model):
+    name = models.CharField(max_length=100)
+    books = models.ForeignKey(Book, related_name='sheets')
+    page = models.OneToOneField('Page')
+
+
+class Page(models.Model):
+    name = models.CharField(max_length=100)
